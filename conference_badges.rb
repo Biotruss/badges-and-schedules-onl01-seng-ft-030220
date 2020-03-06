9535cb7293d3db2927e30c2bcd9c6c230c152915
Craig Christopher Messi <craig.messi@gmail.com>
2020-03-06 08:22:46 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,16 +19,13 @@ def assign_rooms(name)
 end
 
 def printer(attendees)
-  
-  welcome = []
-  room = 1
-    attendees.each do |person|
-      "Hello, my name is #{person}."
-      "Hello, #{person}! You'll be assigned to room #{room}!"
-      room += 1
-    end
-  welcome
-  
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end
+
+  assign_rooms(attendees).each do |room|
+    puts room
+  end
 end
   
   
