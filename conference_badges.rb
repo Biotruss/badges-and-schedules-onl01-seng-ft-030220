22c758e65bd6b8895a1245de44e0f857653e03c9
Craig Christopher Messi <craig.messi@gmail.com>
2020-03-06 08:14:42 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,33 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name)
+  batch = []
+  name.each { |badge| batch << "Hello, my name is #{badge}."}
+  batch
+end
+
+def assign_rooms(name)
+  welcome = []
+  room = 1
+    name.each do |person|
+      welcome << "Hello, #{person}! You'll be assigned to room #{room}!"
+      room += 1
+    end
+  welcome
+end
+
+def printer(attendees)
+  
+end
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
